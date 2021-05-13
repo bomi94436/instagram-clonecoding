3c4a385896bf7b7d4a502dc51875320534ffe6f8
bomi94436 <boo072963@naver.com>
2021-05-14 05:35:43 +0900

    MODIFY #3: 닉네임 중복체크 추가

--- back/src/services/user.ts
+++ back/src/services/user.ts
@@ -14,15 +14,24 @@ exports.postUser = async (userData: User): Promise<User> => {
   );
   if (exUser) throw new CustomError(409, '이미 사용중인 아이디입니다.');
 
+  const [err2, exUser2] = await to(
+    User.findOne({
+      where: {
+        nickname: userData.nickname,
+      },
+    })
+  );
+  if (exUser2) throw new CustomError(409, '이미 사용중인 닉네임입니다.');
+
   const hashedPassword = await bcrypt.hash(userData.password, 12);
-  const [err2, user] = await to(
+  const [err3, user] = await to(
     User.create({
       email: userData.email,
       password: hashedPassword,
       nickname: userData.nickname,
     })
   );
-  if (err2) throw Error(err2.message);
+  if (err3) throw Error(err3.message);
 
   return user;
 };
